3bc51c2111c910f86580d3ea5ea03dfb7fb3d8d4
ExpoSeed <->
2020-12-02 22:14:52 -0600

    Day 2 without the generator

--- src/day2.rs
+++ src/day2.rs
@@ -5,7 +5,8 @@ pub struct Password {
     password: String,
 }
 
-#[aoc_generator(day2)]
+#[aoc_generator(day2, part1, with_generator)]
+#[aoc_generator(day2, part2, with_generator)]
 pub fn input_generator(input: &str) -> Vec<Password> {
     input
         .lines()
@@ -22,7 +23,7 @@ pub fn input_generator(input: &str) -> Vec<Password> {
         .collect()
 }
 
-#[aoc(day2, part1)]
+#[aoc(day2, part1, with_generator)]
 pub fn solve_part1(passwords: &[Password]) -> u32 {
     passwords
         .iter()
@@ -36,7 +37,7 @@ pub fn solve_part1(passwords: &[Password]) -> u32 {
         })
 }
 
-#[aoc(day2, part2)]
+#[aoc(day2, part2, with_generator)]
 pub fn solve_part2(passwords: &[Password]) -> u32 {
     passwords
         .iter()
@@ -44,6 +45,47 @@ pub fn solve_part2(passwords: &[Password]) -> u32 {
             let bytes = entry.password.as_bytes();
             count + if (bytes[entry.num1 as usize - 1] as char == entry.letter) ^ 
                        (bytes[entry.num2 as usize - 1] as char == entry.letter) {
+                1
+            } else {
+                0
+            }
+        })
+}
+
+#[aoc(day2, part1, no_generator)]
+pub fn solve_part1_no_generator(input: &str) -> u32 {
+    input
+        .lines()
+        .fold(0, |count, line| {
+            let v: Vec<&str> = line.split(" ").collect();
+            let bounds: Vec<&str> = v[0].split("-").collect();
+            let min = bounds[0].parse().unwrap();
+            let max = bounds[1].parse().unwrap();
+            let letter = v[1].chars().next().unwrap();
+            let password = String::from(v[2]);
+            let letter_count = password.matches(letter).count() as u32;
+            count + if letter_count >= min && letter_count <= max {
+                1
+            } else {
+                0
+            }
+        })
+}
+
+#[aoc(day2, part2, no_generator)]
+pub fn solve_part2_no_generator(input: &str) -> u32 {
+    input
+        .lines()
+        .fold(0, |count, line| {
+            let v: Vec<&str> = line.split(" ").collect();
+            let bounds: Vec<&str> = v[0].split("-").collect();
+            let index1: usize = bounds[0].parse().unwrap();
+            let index2: usize = bounds[1].parse().unwrap();
+            let letter = v[1].chars().next().unwrap();
+            let password = String::from(v[2]);
+            let bytes = password.as_bytes();
+            count + if (bytes[index1 - 1] as char == letter) ^ 
+                       (bytes[index2 - 1] as char == letter) {
                 1
             } else {
                 0

5872eaca7b5f25a4ec90cbe5b86536521fc7a4eb
ExpoSeed <->
2020-12-05 00:07:38 -0600

    Cleanup day 5

--- src/day5.rs
+++ src/day5.rs
@@ -3,32 +3,7 @@ pub fn solve_part1(input: &str) -> usize {
     input
         .lines()
         .fold(0, |current_max, line| {
-            let mut iter = line.chars();
-            let row = {
-                let mut lower_bound = 0;
-                let mut upper_bound = 128;
-                while lower_bound != upper_bound - 1 {
-                    match iter.next() {
-                        Some('B') => lower_bound = (upper_bound + lower_bound)/2,
-                        Some('F') => upper_bound = (upper_bound + lower_bound)/2,
-                        _ => panic!("bad row input"),
-                    };
-                };
-                lower_bound
-            };
-            let col = {
-                let mut lower_bound = 0;
-                let mut upper_bound = 8;
-                while lower_bound != upper_bound - 1 {
-                    match iter.next() {
-                        Some('R') => lower_bound = (upper_bound + lower_bound)/2,
-                        Some('L') => upper_bound = (upper_bound + lower_bound)/2,
-                        _ => panic!("bad col input"),
-                    };
-                };
-                lower_bound
-            };
-            let seat_id = (row * 8) + col;
+            let seat_id = get_seat_id(line);
             if seat_id > current_max {
                 seat_id
             } else {
@@ -41,34 +16,7 @@ pub fn solve_part1(input: &str) -> usize {
 pub fn solve_part2(input: &str) -> usize {
     let ids: Vec<usize> = input
         .lines()
-        .map(|line| {
-            let mut iter = line.chars();
-            let row = {
-                let mut lower_bound = 0;
-                let mut upper_bound = 128;
-                while lower_bound != upper_bound - 1 {
-                    match iter.next() {
-                        Some('B') => lower_bound = (upper_bound + lower_bound)/2,
-                        Some('F') => upper_bound = (upper_bound + lower_bound)/2,
-                        _ => panic!("bad row input"),
-                    };
-                };
-                lower_bound
-            };
-            let col = {
-                let mut lower_bound = 0;
-                let mut upper_bound = 8;
-                while lower_bound != upper_bound - 1 {
-                    match iter.next() {
-                        Some('R') => lower_bound = (upper_bound + lower_bound)/2,
-                        Some('L') => upper_bound = (upper_bound + lower_bound)/2,
-                        _ => panic!("bad col input"),
-                    };
-                };
-                lower_bound
-            };
-            (row * 8) + col
-        })
+        .map(|line| get_seat_id(line))
         .collect();
 
     for (index1, element1) in (&ids[0..ids.len()-1]).iter().enumerate() {
@@ -82,4 +30,33 @@ pub fn solve_part2(input: &str) -> usize {
         }
     }
     unreachable!()
+}
+
+fn get_seat_id(line: &str) -> usize {
+    let mut iter = line.chars();
+    let row = {
+        let mut lower_bound = 0;
+        let mut upper_bound = 128;
+        while lower_bound != upper_bound - 1 {
+            match iter.next() {
+                Some('B') => lower_bound = (upper_bound + lower_bound)/2,
+                Some('F') => upper_bound = (upper_bound + lower_bound)/2,
+                _ => panic!("bad row input"),
+            };
+        };
+        lower_bound
+    };
+    let col = {
+        let mut lower_bound = 0;
+        let mut upper_bound = 8;
+        while lower_bound != upper_bound - 1 {
+            match iter.next() {
+                Some('R') => lower_bound = (upper_bound + lower_bound)/2,
+                Some('L') => upper_bound = (upper_bound + lower_bound)/2,
+                _ => panic!("bad col input"),
+            };
+        };
+        lower_bound
+    };
+    (row * 8) + col
 }
